28058ba05309eccb2dd8cf0367122ccbfff14e73
bobin17231 <bobinabraham5@gmail.com>
2026-02-01 15:32:48 -0500

    config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine my beautiful Sundari 🌹", // The big question!
+            text: "Will you be my Valentine my beautiful Sundari.. 🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -51,7 +51,7 @@ const CONFIG = {
     celebration: {
         title: "Yay! I'm the luckiest man in the world! 🎉💝💖💝💓",
         message: "Now come get your gift, a big umma!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        emojis: "🎁💖💚🤗💙💝🩷💋❤️💚💕💙"  // These will bounce around
     },
 
     // Color scheme for the website
